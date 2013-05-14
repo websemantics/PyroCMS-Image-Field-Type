671ce509c4c28912ed24819406cd754306e9f4f5
Adam Fairholm <adam.fairholm@gmail.com>
2013-05-14 10:51:15 -0400

    Updating to now allow AJAX file uploads.

--- field.image.php
+++ field.image.php
@@ -1,7 +1,16 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
- * PyroStreams Image Field Type
+ * Streams Image Field Type
+ *
+ * The streams image field type works by uploading
+ * an image to a the folder of your choice, and then
+ * setting a hidden input field with the image Id. An image is removed
+ * by setting a null value for the hidden input.
+ *
+ * The image field type is integrated closely with the PyroCMS 
+ * files system, so all files that are uploaded have an entry
+ * in the files table and show up in the files module.
  *
  * @package		PyroCMS\Core\Modules\Streams Core\Field Types
  * @author		PyroCMS Dev Team
@@ -27,6 +36,17 @@ class Field_image
 	{
 		get_instance()->load->library('image_lib');
 	}
+
+	public function event()
+	{
+		$this->CI->type->add_js('image', 'jquery.ui.widget.js');
+		$this->CI->type->add_js('image', 'jquery.iframe-transport.js');
+		$this->CI->type->add_js('image', 'jquery.fileupload.js');
+		$this->CI->type->add_js('image', 'imagefield.js');
+		
+		$this->CI->type->add_css('image', 'imagefield.css');		
+	}
+
 	/**
 	 * Output form input
 	 *
@@ -34,77 +54,45 @@ class Field_image
 	 * @param	array
 	 * @return	string
 	 */
-	public function form_output($params)
+	public function form_output($params, $entry_id, $field)
 	{
 		$this->CI->load->config('files/files');
 
-		$out = '';
+		// Get the field ID encrypted
+		$this->CI->load->library('Encrypt');
+		$field_id = $this->CI->encrypt->encode($field->field_id);
+
+		$out = '<div class="img_upload_wrap" id="'.$params['form_slug'].'_img_upload_wrap">';
+
+		// Add the field-id as a hidden input so we can
+		// access it via jQuery.
+		$out .= '<input type="hidden" id="'.$params['form_slug'].'_field_id" value="'.$field_id.'" />';
+
+		// Progress bar.
+		$out .= '<div class="progress"><div class="bar" style="width: 0%;"></div></div>';
 
 		// if there is content and it is not dummy or cleared
 		if ($params['value'] and $params['value'] != 'dummy')
 		{
-			$out .= '<span class="image_remove">X</span><a class="image_link" href="'.site_url('files/large/'.$params['value']).'" target="_break"><img src="'.site_url('files/thumb/'.$params['value']).'" /></a><br />';
-			$out .= form_hidden($params['form_slug'], $params['value']);
-		}
-		else
-		{
-			$out .= form_hidden($params['form_slug'], 'dummy');
+			$out .= '<span class="image_remove" data-id="'.$params['form_slug'].'">X</span><a class="image_link" href="'.site_url('files/large/'.$params['value']).'" target="_break"><img src="'.site_url('files/thumb/'.$params['value']).'" class="img_ft_thumb" /></a><br />';
+			return $out .= form_hidden($params['form_slug'], $params['value']).'</div>';
 		}
 
-		$options['name'] 	= $params['form_slug'];
-		$options['name'] 	= $params['form_slug'].'_file';
-		$this->CI->type->add_js('image', 'imagefield.js');
-		$this->CI->type->add_css('image', 'imagefield.css');
-		return $out .= form_upload($options);
-	}
+		//$folder_id = $params['custom']['folder'];
 
-	/**
-	 * Process before saving to database
-	 *
-	 * @param	array
-	 * @param	obj
-	 * @return	string
-	 */
-	public function pre_save($input, $field, $stream, $row_id, $form_data)
-	{
-		// If we do not have a file that is being submitted. If we do not,
-		// it could be the case that we already have one, in which case just
-		// return the numeric file record value.
-		if ( ! isset($_FILES[$field->field_slug.'_file']['name']) or ! $_FILES[$field->field_slug.'_file']['name'])
-		{
-			// allow dummy as a reset
-			if (isset($form_data[$field->field_slug]) and $form_data[$field->field_slug])
-			{
-				return $form_data[$field->field_slug];
-			}
-			else
-			{
-				return null;
-			}
-		}
+		/*<!--<a href="#" data-id="'.$params['form_slug'].'" data-folder-id="'.$folder_id.'" class="choose-img-js button">Choose Image</a>-->*/
 
-		$this->CI->load->library('files/files');
+		$out .= '<p><input id="'.$params['form_slug'].'" type="file" class="upload-img-js" name="'.$params['form_slug'].'_image_input" data-field-id="'.$field_id.'" /></p>';
 
-		// Resize options
-		$resize_width 	= (isset($field->field_data['resize_width'])) ? $field->field_data['resize_width'] : null;
-		$resize_height 	= (isset($field->field_data['resize_height'])) ? $field->field_data['resize_height'] : null;
-		$keep_ratio 	= (isset($field->field_data['keep_ratio']) and $field->field_data['keep_ratio'] == 'yes') ? true : false;
+		//$out .= '<input type="hidden" name="'.$params['form_slug'].'_field" value="'.$field_id.'" />';
 
-		// If you don't set allowed types, we'll set it to allow all.
-		$allowed_types 	= (isset($field->field_data['allowed_types'])) ? $field->field_data['allowed_types'] : '*';
+		$out .= '</div>';
 
-		$return = Files::upload($field->field_data['folder'], null, $field->field_slug.'_file', $resize_width, $resize_height, $keep_ratio, $allowed_types);
+		//$out .= '<p><a href="'..'" data-id="'.$params['form_slug'].'" data-folder-id="'.$folder_id.'" class="button modal">+ Upload</a></p>';
 
-		if ( ! $return['status'])
-		{
-			$this->CI->session->set_flashdata('notice', $return['message']);
-			return null;
-		}
-		else
-		{
-			// Return the ID of the file DB entry
-			return $return['data']['id'];
-		}
+		//$out .= '<ul id="'.$params['form_slug'].'_file_choose"></ul>';
+
+		return $out;
 	}
 
 	/**
@@ -115,12 +103,18 @@ class Field_image
 	 */
 	public function pre_output($input, $params)
 	{
-		if ( ! $input or $input == 'dummy' ) return null;
+		if ( ! $input or $input == 'dummy' ) {
+			return null;
+		}
 
 		// Get image data
-		$image = $this->CI->db->select('filename, alt_attribute, description, name')->where('id', $input)->get('files')->row();
+		$image = $this->CI->db
+						->select('filename, alt_attribute, description, name')
+						->where('id', $input)->get('files')->row();
 
-		if ( ! $image) return null;
+		if ( ! $image) {
+			return null;
+		}
 
 		// This defaults to 100px wide
 		return '<img src="'.site_url('files/thumb/'.$input).'" alt="'.$this->obvious_alt($image).'" />';
@@ -287,6 +281,69 @@ class Field_image
 			return $image->description;
 		}
 		return $image->name;
+	}
+
+	/**
+	 * Upload Function
+	 *
+	 * Accessed via AJAX.
+	 *
+	 * @return json
+	 */
+	public function ajax_upload()
+	{
+		// To be returned via JSON
+		$return = array();
+
+		// We need a file.
+		if ( ! $_FILES) {
+			$return['error'] = 'No file provided';
+		}
+
+		// Get/decode the field_id
+		$field_id_raw = $this->CI->input->post('field_id');
+
+		if ( ! $field_id_raw) {
+			$return['error'] = 'No field ID provided.';
+		}
+
+		// Decode field ID
+		$this->CI->load->library('Encrypt');
+		$field_id = $this->CI->encrypt->decode($field_id_raw);
+
+		if ( ! $field_id) {
+			$return['error'] = 'No field ID provided.';
+		}
+
+		// Get the field.
+		if ( ! $field = $this->CI->fields_m->get_field($field_id)) {
+			$return['error'] = 'Invalid field ID.';
+		}
+
+		// Looks like we have a field - we're ready to
+		// upload the file based on the field's settings.
+		$this->CI->load->library('files/files');
+
+		// Resize options
+		$resize_width 	= (isset($field->field_data['resize_width'])) ? $field->field_data['resize_width'] : null;
+		$resize_height 	= (isset($field->field_data['resize_height'])) ? $field->field_data['resize_height'] : null;
+		$keep_ratio 	= (isset($field->field_data['keep_ratio']) and $field->field_data['keep_ratio'] == 'yes') ? true : false;
+
+		// If you don't set allowed types, we'll set it to allow all.
+		$allowed_types 	= (isset($field->field_data['allowed_types'])) ? $field->field_data['allowed_types'] : '*';
+
+		$upload = Files::upload($field->field_data['folder'], null, $field->field_slug.'_image_input', $resize_width, $resize_height, $keep_ratio, $allowed_types);
+
+		if ( ! $upload['status']) {
+			$return['error'] = $upload['message'];
+		} else {
+			// Return the ID of the file DB entry
+			$return['uploadId'] = $upload['data']['id'];
+		}
+	
+		// Return the JSON object.
+		header('Content-type: application/json');
+		echo json_encode($return);
 	}
 
 }
--- js/imagefield.js
+++ js/imagefield.js
@@ -1,11 +1,148 @@
 $(function() {
-  $('.image_remove').click(function(e){
-    // change the input to cleared so we know its saved
-    $(this).siblings('input[type="hidden"]').attr('value', 'dummy');
-    // remove the a tag
-    $(this).siblings('a').remove();
-    // remove this close button
-    $(this).remove();
-    return false;
-  });
+
+	function bindFileUpload(obj)
+	{
+  	  	/**
+	  	 * Image Upload Function
+	  	 */
+	    obj.fileupload({
+
+	        dataType: 'json',
+	        url: SITE_URL+'streams_core/public_ajax/field/image/upload',
+	        progressall: function (e, data) {
+
+	   			var id = $(this).attr('id');
+	       		var progress = parseInt(data.loaded / data.total * 100, 10);
+	        	$('#'+id+'_img_upload_wrap .progress .bar').css(
+	            	'width',
+	            	progress + '%'
+	        	);
+	    	},
+	        add: function (e, data) {
+
+	            // Add formdata
+	            data.formData = {field_id: $(this).attr('data-field-id')};
+
+	            data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+	            data.submit();
+	        },
+	        send: function (e, data) {
+	        	// Now that we're uploading, show the progress bar.
+	        	var id = $(this).attr('id');
+				$('#'+id+'_img_upload_wrap .progress').css('display', 'block');
+	        },
+	        done: function (e, data) {
+
+	 			if (data.result.error) {
+	 				data.context.text(data.result.error);
+	 			} else {
+
+	   				var id = $(this).attr('id');
+
+	   				// Add a hidden input with the value
+	   				// of the uploaded image.
+	   				$('#'+id+'_img_upload_wrap .progress').append(
+	   					'<input type="hidden" name="'+
+	   					id+'" value="'+data.result.uploadId+'" />');
+
+	   				// We don't need to see the progress bar anymore.
+					$('#'+id+'_img_upload_wrap .progress').css('display', 'none');
+
+	   				// Prepend the image.
+	   				$('#'+id+'_img_upload_wrap').prepend(
+	   					'<p><span class="image_remove" data-id="'+id+'">X</span><img src="'+SITE_URL+'files/thumb/'+data.result.uploadId+'" class="img_ft_thumb" /></p>');
+
+	 				// Remove any messages we were seeing.
+	 				data.context.text('');
+	 			}
+	     
+	        }
+
+	    });
+
+	}
+
+	bindFileUpload($('.upload-img-js'));
+
+  	/**
+  	 * Choose Image Function
+  	 */
+	$('.choose-img-js').click(function(e){
+
+		e.preventDefault();
+
+		var id = $(this).attr('data-id');
+		var folder_id = $(this).attr('data-folder-id');
+		var folders_center = $('#'+id+'_file_choose');
+
+		post_data = {parent: folder_id};
+		$.post(SITE_URL+'admin/files/folder_contents', post_data, function(data){
+			
+			var results = $.parseJSON(data);
+
+			if (results.status) {
+
+				// iterate so that we have folders first, files second
+				/*$.each(results.data, function(type, data){
+					$.each(data, function(index, item){
+						item.el_type = type;
+						items.push(item);
+					});
+				});*/
+
+				for (var i = 0; i < results.data.file.length; i++)
+				{
+					var item = results.data.file[i];
+					folders_center.append(
+						'<li class="file" data-id="'+item.id+'" data-name="'+item.name+'">'+
+							'<img src="'+SITE_URL+'files/cloud_thumb/'+item.id+'?'+new Date().getMilliseconds()+'" alt="'+item.name+'"/>'+
+							'<span class="name-text">'+item.name+'</span>'+
+						'</li>'
+					);
+				}
+			}
+
+			console.log(results);
+		
+		});
+
+	});
+
+	
+	/**
+	 * Remove Image Button
+	 *
+	 * Removes the image and sets a hidden
+	 * input to a null value.
+	 */
+	$('.img_upload_wrap').on('click', '.image_remove', function(r)
+	{
+		r.preventDefault();
+		var id = $(this).attr('data-id');
+		var wrap = $('#'+id+'_img_upload_wrap');
+		var fId = $('#'+id+'_field_id').val();
+
+		// Change the input to empty
+		$(this).siblings('input[type="hidden"]').attr('value', '');
+
+		// Make the upload input visible again.
+		$('#'+id).css('display', 'block');
+
+		// remove the image preview
+		$(this).siblings('.img_ft_thumb').remove();
+		$(this).siblings('a').remove();
+
+		// Add a file upload button.
+		wrap.prepend('<p><input id="'+id+'" type="file" class="upload-img-js" name="'+id+'_image_input" data-field-id="'+fId+'" /></p>');
+
+		// Bind fileupload again
+		bindFileUpload($('#'+id));
+
+		// remove this close button
+		$(this).remove();
+		
+		return false;
+	
+	});
+
 });
